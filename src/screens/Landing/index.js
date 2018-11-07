becfa21f73f15f8056f0a6cb6fc39e36a3a470d5
JJ Ashcraft <jj@jjashcraft.com>
2018-11-07 18:57:33 -0500

    Added basic signup form, header and signup form css, updated landing page route

--- src/screens/Landing/index.js
+++ src/screens/Landing/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-import Header from "../../components/Header/";
+import Header from "../../components/Header";
+import Signup from "../../components/Signup";
 import PropTypes from "prop-types";
 
 //this screen should return components necessary to build the landing page.
 
 class ScreensLanding extends Component {
   render() {
-    return <Header />;
+    return (
+      <React.Fragment>
+        <Header />
+        <Signup />
+      </React.Fragment>
+    );
   }
 }
 ScreensLanding.propTypes = {};
