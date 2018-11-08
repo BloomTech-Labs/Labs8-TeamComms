28752eed4b90271233565f0a3cd9a22a1881df01
-tristan- <tristan.linehan@gmail.com>
2018-11-08 06:49:07 -0800

    added redux store, added redux chrome tools, added test components for redux, restructured file format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
+
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import ScreensLanding from "./screens/Landing";
+import ReduxTest from './components/Test/ReduxTest'
+import { connect } from 'react-redux';
+import  { appMounted }  from './actions/index';
 import "./App.css";
 
+
 class App extends Component {
+
+  componentDidMount() {
+    this.props.appMounted();
+  }
+
   render() {
     return (
-      <div className="App">
-        Ready to go.
-        <Route
-          exact
-          path="/landing"
-          render={props => {
-            return <ScreensLanding />;
-          }}
-        />
+      <div className="App">       
+
+          Ready to go. 
+
+          <Switch>
+            <Route
+              exact
+              path="/landing"
+              render={props => {
+                return <ScreensLanding />;
+              }} 
+            />
+        
+          <Route exact path="/reduxtest" component={ReduxTest} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+
+const mapStateToProps = state => {
+  return state;
+};  
+export default connect(mapStateToProps, { appMounted })(App);  
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,13 @@
+
 import { combineReducers } from 'redux';
 
-//below 1 line, boilerplate
-import { fetchAllMissionsReducer } from './fetchAllMissionsReducer'
-
-export const initialState = {
-    missions: [],
-    isUserLoggedIn: false,
-    saveInProgress: false,
-    saved: false,
-    fetchingMissions: false,
-    displayingAllMissions: false,
-}
+import  { appMounted }  from './appMountedReducer';
+import { reduxTest } from './reduxTestReducer';
 
 
-export default combineReducers({
-    //below 1 line, boilerplate
-    fetchAllMissionsReducer,
-    // ...
-
+const rootReducer =  combineReducers({
+    appMounted, 
+    reduxTest
 })
+export default rootReducer;
 
