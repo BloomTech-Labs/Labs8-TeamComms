b87988045a017818968f5478e2a84ce4c3789f93
Jameson13B <jamesonbrown@neolivity.com>
2018-11-13 13:18:03 -0700

    Refactored login form to use styled components

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,8 +1,45 @@
 import React, { Component, Fragment } from "react";
 import { callLogIn } from "../../actions/index";
 import { connect } from "react-redux";
+import styled from "styled-components";
+import { CustomInput, PrimaryButton } from "../Common";
 
-import "./login.css";
+const Main = styled.div`
+  margin: 0 auto;
+  padding: 5px 0 5px 0;
+  background: #374353;
+`;
+
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  margin: 0 auto;
+`;
+
+const CustomInputTop = styled(CustomInput)`
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`;
+
+const CustomInputBottom = styled(CustomInput)`
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  border-bottom: none;
+`;
+
+const LoginButton = styled(PrimaryButton)`
+  width: 300px;
+  height: 75px;
+  color: white;
+  border-radius: 5px;
+  background: #25bea0;
+  border: 1px solid grey;
+  font-size: 28px;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  border: none;
+`;
 
 class Login extends Component {
   constructor(props) {
@@ -32,39 +69,32 @@ class Login extends Component {
     };
     return (
       <Fragment>
-        <div className="main">
-          <form
+        <Main>
+          <FormWrapper
             method="post"
-            className="form-wrapper"
             onSubmit={e => {
               this.handleLogInSubmit(e, userInput);
             }}
           >
-            <img src="./images/logo.png" alt="" />
-            <br />
-            <input
+            <CustomInputTop
               placeholder="username"
-              className="custominput-top"
               required
               type="text"
               onChange={this.changeHandler}
               name="username"
               value={this.state.username}
             />{" "}
-            <input
+            <CustomInputBottom
               placeholder="password"
-              className="custominput-bottom"
               required
               type="password"
               name="password"
               onChange={this.changeHandler}
               value={this.state.password}
             />
-            <button type="submit" className="signin-button">
-              Sign In
-            </button>
-          </form>
-        </div>
+            <LoginButton type="submit">Sign In</LoginButton>
+          </FormWrapper>
+        </Main>
       </Fragment>
     );
   }
