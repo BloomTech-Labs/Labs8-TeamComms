fff1e70d4a43f28949621b4649684ce488558974
Jameson13B <jamesonbrown@neolivity.com>
2018-11-14 11:45:51 -0700

    Converted to stateless component

--- src/components/Stripe/index.js
+++ src/components/Stripe/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
 
-class Stripe extends Component {
-  onToken = token => {
+const Stripe = () => {
+  const onToken = token => {
     // Stripe token is generated automatically and passed as props to this function
     const body = JSON.stringify(token);
     // We make an call to an endpoint on our server and send the token
@@ -18,22 +18,19 @@ class Stripe extends Component {
         alert("Payment Error");
       });
   };
-
-  render() {
-    return (
-      <StripeCheckout
-        label="Go Premium" //Original button
-        name="Team Communicator"
-        description="Premium Account"
-        panelLabel="Go Premium" //Submit button
-        amount={1499} //Amount in cents $14.99
-        billingAddress={false} //Turn on to collect address *recommended(false for testing)
-        allowRememberMe={false}
-        token={this.onToken}
-        stripeKey="pk_test_ZU3mlTy0q00DATc9EyF9A8jX"
-      />
-    );
-  }
-}
+  return (
+    <StripeCheckout
+      label="Go Premium" //Original button
+      name="Team Communicator"
+      description="Premium Account"
+      panelLabel="Go Premium" //Submit button
+      amount={999} //Amount in cents $9.99
+      billingAddress={false} //Turn on to collect address *recommended(false for testing)
+      allowRememberMe={false}
+      token={onToken}
+      stripeKey="pk_test_ZU3mlTy0q00DATc9EyF9A8jX"
+    />
+  );
+};
 
 export default Stripe;
