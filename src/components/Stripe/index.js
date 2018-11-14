732269b55e4aef51e46de202bc6e9bd08cef0e37
Jameson13B <jamesonbrown@neolivity.com>
2018-11-14 10:36:07 -0700

    Updated modal props and added comments

--- src/components/Stripe/index.js
+++ src/components/Stripe/index.js
@@ -4,10 +4,11 @@ import axios from "axios";
 
 class Stripe extends Component {
   onToken = token => {
+    // Stripe token is generated automatically and passed as props to this function
     const body = JSON.stringify(token);
-    // Update the endpoint below to our correct server endpoint
+    // We make an call to an endpoint on our server and send the token
     axios
-      .post("/api/stripe/charge", body)
+      .post("/api/stripe/payment", body)
       .then(response => {
         console.log(response);
         alert("Payment Success");
@@ -25,8 +26,8 @@ class Stripe extends Component {
         name="Team Communicator"
         description="Premium Account"
         panelLabel="Go Premium" //Submit button
-        amount={999} //Amount in cents
-        billingAddress={true}
+        amount={1499} //Amount in cents $14.99
+        billingAddress={false} //Turn on to collect address *recommended(false for testing)
         allowRememberMe={false}
         token={this.onToken}
         stripeKey="pk_test_ZU3mlTy0q00DATc9EyF9A8jX"
