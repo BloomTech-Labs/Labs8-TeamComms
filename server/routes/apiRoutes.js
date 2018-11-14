1463a4e290cfbc6d4f8cc4b9ca265310c9dd299e
Jameson13B <jamesonbrown@neolivity.com>
2018-11-14 16:21:40 -0700

    Added /stripe endpoint

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const userRoutes = require("./userRoutes");
 const authRoutes = require("./authRoutes");
+const stripeRoutes = require("./stripeRoutes");
 
 const router = express.Router();
 
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
+router.use("/stripe", stripeRoutes);
 // router.use("/convo", convoRoutes);
 
 module.exports = router;
