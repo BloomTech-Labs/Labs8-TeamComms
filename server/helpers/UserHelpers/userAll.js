83001d9e17e1a3b68ec4b59425232337ca11bcf0
flintbean <ajmerando1@yahoo.com>
2018-11-27 13:28:37 -0800

    userall tweak and added authenticate to endpoint

--- server/helpers/UserHelpers/userAll.js
+++ server/helpers/UserHelpers/userAll.js
@@ -1,20 +1,15 @@
 const User = require("../../models/UserModel");
 
 const userAll = async (req, res) => {
-  await User.find({}, "email displayName organization").exec((err, users) => {
-    if (err) {
-      console.log(err);
-    } else {
-      const newUsers = users.map(user => {
-        return {
-          email: user.email,
-          displayName: user.displayName,
-          organization: user.organization
-        };
-      });
-      res.status(200).send(newUsers);
+  await User.find({}, "-_id email displayName organization").exec(
+    (err, users) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).send(users);
+      }
     }
-  });
+  );
 };
 
 module.exports = userAll;
