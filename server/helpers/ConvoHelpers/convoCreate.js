505617045c010984acd9fe60f5bc5981b42cf1ce
-tristan- <tristan.linehan@gmail.com>
2018-11-27 09:59:57 -0800

    added moment package to server, parsing input from from post to date obj, added error handling

--- server/helpers/ConvoHelpers/convoCreate.js
+++ server/helpers/ConvoHelpers/convoCreate.js
@@ -1,29 +1,39 @@
 const User = require("../../models/UserModel"); //Model
 const Convo = require("../../models/ConvoModel"); //Model
+const moment = require('moment');
 
 const convoCreate = async(req, res) => {
   const user = req.user;
   console.log('user, from convoCreate:', user);
   const newConvo = req.body;
-
-  const convo = new Convo(
-    {
-    creatorId: user._id,
-    title: newConvo.title,
-    description: newConvo.description,
-    // start_time: newConvo.startTime,
-    // end_time: newConvo.endTime,
-    // attendees: 
+  const start = moment(newConvo.startTime).toDate();
+  const end = moment(newConvo.endTime).toDate();
+  try {
+    const convo = new Convo(
+      {
+      creatorId: user._id,
+      title: newConvo.title,
+      description: newConvo.description,
+      start_time: start,
+      end_time: end,
+      repeat: newConvo.repeat,
+      // questions: 
+      // attendees: 
+      }
+    );
+    if (!convo) {
+      throw new Error('Convo creation failed!')
+    } 
+    else {
+      await convo.save();
+      user.meetings.push(convo._id);
+      user.created_meetings.push(convo._id);
+      await user.save();
+      res.status(201).send(convo);
     }
-  );
-  await convo.save();
-  user.meetings.push(convo._id);
-  user.created_meetings.push(convo._id);
-  await user.save();
-  res.status(201).send({
-    user: {
-      meeting: user.meetings
-    }
-  });
+  }
+  catch(err) {
+    res.status(400).send(err)
+  }
 }
 module.exports = convoCreate;
