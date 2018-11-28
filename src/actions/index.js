330f4c47b652171cf96dfdb10afa1691eb2ce2f4
Jameson13B <jamesonbrown@neolivity.com>
2018-11-28 11:09:05 -0700

    Created the actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import { callLogIn } from "./callLogIn";
 import { loadInitialDataSocket } from "./loadInitialDataSocket";
 import { toggleOverpane } from "./toggleOverpane";
 import { callLogOut } from "./callLogOut";
+import { callCreate } from "./callCreate";
 
 export { callReg } from "./callReg";
 export { callLogIn } from "./callLogIn";
@@ -10,3 +11,4 @@ export { callUpdate } from "./callUpdate";
 export { loadInitialDataSocket } from "./loadInitialDataSocket";
 export { toggleOverpane } from "./toggleOverpane";
 export { callLogOut } from "./callLogOut";
+export { callCreate } from "./callCreate";
