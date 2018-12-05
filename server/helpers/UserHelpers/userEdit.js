3b8351aa2c112730dc330a0cf13f1084e0685ba7
Jameson13B <jamesonbrown@neolivity.com>
2018-12-05 14:24:56 -0700

    Updates to the userEdit endpoint

--- server/helpers/UserHelpers/userEdit.js
+++ server/helpers/UserHelpers/userEdit.js
@@ -3,10 +3,12 @@ const tokenCheck = require("../../validation/tokenCheck");
 const hashedPassword = require("../../validation/hashedPassword");
 
 const userEdit = async (req, res) => {
-  let token = req.headers["jwt"] || req.headers['Authorization'] || req.headers['auth'] || req.headers['x-access-token'];
-  let newInfo = req.body; //TODO - Talk with front end about what info is being passed in
-  if(Object.keys(req.body).length < 4){
-    return res.send("Please send {displayName, givenName, familyName, email, oldPw, newPw} even if empty string");
+  let newInfo = req.body;
+  const reqUser = req.user;
+  if (Object.keys(req.body).length < 4) {
+    return res.send(
+      "Please send {displayName, givenName, familyName, email, oldPw, newPw} even if empty string"
+    );
   }
 
   if (!newInfo.phone_number.length) {
@@ -36,38 +38,16 @@ const userEdit = async (req, res) => {
   }
 
   try {
-    //Input - Token into decoder and checks if secret is valid
-    //Output - Decoded token or null if invalid
-    token = tokenCheck(token);
-
-    if (!token) {
-      throw new Error("Invalid token!");
-    } else {
-      //Input - Checks user collection for user using id from decoded token payload then updates
-      //Output - New user object with updated info
-      const user = await User.findOneAndUpdate(
-        { _id: token.id },
-        { $set: newInfo },
-        (err, result) => {
-          if (err) {
-            throw new Error("Problem with editing user");
-          } else {
-            return result;
-          }
-        }
-      );
-
-      res.status(200).send({
-        user: {
-          displayName: user.displayName,
-          name: user.name,
-          email: user.email,
-          phone_number: user.phone_number,
-          organization: user.organization,
-          premium: user.premium
+    const user = await User.findByIdAndUpdate(reqUser._id, { newInfo })
+      .populate("-password -googleId -stripeId, -zoomId")
+      .exec((err, result) => {
+        if (err) {
+        } else {
+          return result;
         }
       });
-    }
+
+    res.status(200).send(user);
   } catch (err) {
     res.status(401).send(err.message);
   }
