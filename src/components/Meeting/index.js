0c94b50a92e8237595fd27f56516b468e3ba58c3
flintbean <ajmerando1@yahoo.com>
2018-12-14 10:19:47 -0800

    fixed bug to text persistence. Removed alert on save

--- src/components/Meeting/index.js
+++ src/components/Meeting/index.js
@@ -206,17 +206,17 @@ class Meeting extends Component {
       zoom: ""
     };
 
-    // const socket_connect = function(room) {
-    //   return io("localhost:8080/meeting", {
-    //     query: "r_var=" + room
-    //   });
-    // };
-
     const socket_connect = function(room) {
-      return io("https://teamcomm2.herokuapp.com/meeting", {
+      return io("localhost:8080/meeting", {
         query: "r_var=" + room
       });
     };
+
+    // const socket_connect = function(room) {
+    //   return io("https://teamcomm2.herokuapp.com/meeting", {
+    //     query: "r_var=" + room
+    //   });
+    // };
 
     const id = this.props.match.params.id;
     socket = socket_connect(id);
@@ -230,9 +230,6 @@ class Meeting extends Component {
 
     socket.on("question", questions => {
       return this.setState({ questions });
-    });
-    socket.on("finalize", () => {
-      alert("Meeting has been saved!");
     });
   }
   componentDidMount() {
@@ -285,6 +282,7 @@ class Meeting extends Component {
 
   finalizeMeeting = e => {
     e.preventDefault();
+    socket.emit("update text", this.state.text)
     socket.emit("finalize");
   };
 
